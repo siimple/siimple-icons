73c408bb6dcf2942f3ff5d8e1ecb6cc3e6bf1f36
jmjuanes <josemijuanes@gmail.com>
2017-05-31 19:02:28 +0200

    gulpfile.js: generate a sprit svg file for all svg icons

---
+++ gulpfile.js
@@ -0,0 +1,43 @@
+//Import dependencies
+var gulp = require('gulp');
+var path = require('path');
+var svgstore = require('gulp-svgstore');
+var svgmin = require('gulp-svgmin');
+
+//Build the svg sprite task
+gulp.task('build', function()
+{
+  //Source files
+  var src = [ 'action/*.svg' ];
+
+  //Get the svg files
+  return gulp.src(src)
+
+  //Minimize each file
+  .pipe(svgmin(function(file)
+  {
+    //Get the file parent folder and name
+    var name = file.path.split(path.sep).slice(-2).join('-');
+
+    //Remove the file extension
+    var prefix = path.basename(name, path.extname(name));
+
+    //Get the icon prefix
+    //var prefix = path.basename(file.relative, path.extname(file.relative));
+
+    //Display in console
+    console.log('Processing ' + prefix);
+
+    //Return the prefix plugin
+    return { plugins: [ { cleanupIDs: { prefix: prefix + '-',  minify: true } } ] };
+  }))
+
+  //Build the sprite
+  .pipe(svgstore())
+
+  //Save the sprite image
+  .pipe(gulp.dest('./'));
+});
+
+//Default task
+gulp.task('default', [ 'build' ]);
