e04ee8afff88ae929829d6e73eaea085295d3d36
jmjuanes <josemijuanes@gmail.com>
2017-06-01 17:02:32 +0200

    Import gulp-rename method. Rename output svg sprite file.

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,17 @@
 //Import dependencies
+var rmr = require('rmr');
 var gulp = require('gulp');
 var path = require('path');
 var svgstore = require('gulp-svgstore');
 var svgmin = require('gulp-svgmin');
+var rename = require("gulp-rename");
+
+//Clean the dist folder
+gulp.task('clean', function()
+{
+  //Clean the dist folder
+  return rmr.sync('./dist');
+});
 
 //Build the svg sprite task
 gulp.task('build', function()
@@ -35,9 +44,12 @@ gulp.task('build', function()
   //Build the sprite
   .pipe(svgstore())
 
+  //Rename the output file
+  .pipe(rename({ basename: 'siimple-icons' }))
+
   //Save the sprite image
-  .pipe(gulp.dest('./'));
+  .pipe(gulp.dest('./dist'));
 });
 
 //Default task
-gulp.task('default', [ 'build' ]);
+gulp.task('default', [ 'clean', 'build' ]);
