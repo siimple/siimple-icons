fcda4e149b2301be4f8f321a69386f0085650a59
jmjuanes <josemijuanes@gmail.com>
2017-06-01 18:10:44 +0200

    Set symbol id using the icon folder and the icon file name

--- gulpfile.js
+++ gulpfile.js
@@ -20,25 +20,32 @@ gulp.task('build', function()
   var src = [ 'action/*.svg', 'alert/*.svg', 'navigation/*.svg' ];
 
   //Get the svg files
-  return gulp.src(src)
+  return gulp.src(src, { base: './' })
+
+  //Rename the file base name
+  .pipe(rename(function(file)
+  {
+    //Remove the file extension and save as the file basename
+    file.basename = file.dirname + '-' + file.basename;
+
+    //Display in console
+    console.log('Processing ' + file.basename);
+  }))
 
   //Minimize each file
   .pipe(svgmin(function(file)
   {
     //Get the file parent folder and name
-    var name = file.path.split(path.sep).slice(-2).join('-');
+    //var name = file.path.split(path.sep).slice(-2).join('-');
 
     //Remove the file extension
-    var prefix = path.basename(name, path.extname(name));
+    //var prefix = path.basename(name, path.extname(name));
 
     //Get the icon prefix
     //var prefix = path.basename(file.relative, path.extname(file.relative));
 
-    //Display in console
-    console.log('Processing ' + prefix);
-
     //Return the prefix plugin
-    return { plugins: [ { cleanupIDs: { prefix: prefix + '-',  minify: true } } ] };
+    return { plugins: [ { cleanupIDs: { prefix: file.relative + '-',  minify: true } } ] };
   }))
 
   //Build the sprite
