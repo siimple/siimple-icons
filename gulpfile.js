e2decf3ee9caafe9e525eb87a4415d5233cc86ea
jmjuanes <josemijuanes@gmail.com>
2017-07-24 13:22:13 +0200

    gulpfile.js: compile examples

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,8 @@ var path = require('path');
 var svgstore = require('gulp-svgstore');
 var svgmin = require('gulp-svgmin');
 var rename = require("gulp-rename");
+var handlebars = require('gulp-compile-handlebars');
+var fs = require('fs');
 
 //Clean the dist folder
 gulp.task('clean', function()
@@ -40,6 +42,34 @@ gulp.task('sprite', function()
 
   //Save the sprite image
   .pipe(gulp.dest('./dist'));
+});
+
+//Create the example files
+gulp.task('examples', function()
+{
+  //Initialize the data object
+  var data = { files: [] };
+
+  //Get the files
+  data.files = fs.readdirSync('./src/').filter(function(el)
+  {
+    return el !== '.DS_Store';
+  }).map(function(el)
+  {
+    return path.basename(el, '.svg');
+  });
+
+  //Select the templates
+  gulp.src('./templates/*.handlebars')
+
+  //Compile using handlebars
+  .pipe(handlebars(data, {}))
+
+  //Rename the files
+  .pipe(rename({ extname: '.html' }))
+
+  //Save to examples
+  .pipe(gulp.dest('./examples/'));
 });
 
 //Default task
