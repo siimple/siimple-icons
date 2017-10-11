fdef7e3c879010eae2a98b610c84b2bf544f25c2
jmjuanes <josemijuanes@gmail.com>
2017-10-11 17:01:16 +0200

    gulpfile.js: added tasks to build font in svg, ttf and woff2 formats

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,72 @@
 //Import dependencies
 var rmr = require('rmr');
-var gulp = require('gulp');
+var fs = require('fs');
 var path = require('path');
+var gulp = require('gulp');
 var svgstore = require('gulp-svgstore');
 var svgmin = require('gulp-svgmin');
 var rename = require("gulp-rename");
 var handlebars = require('gulp-compile-handlebars');
-var fs = require('fs');
+var SVGIcons2SVGFontStream = require('svgicons2svgfont');
+var svg2ttf = require('gulp-svg2ttf');
+var ttf2woff2 = require('gulp-ttf2woff2');
+var utily = require('utily');
 
 //Clean the dist folder
 gulp.task('clean', function()
 {
   //Clean the dist folder
   return rmr.sync('./dist');
+});
+
+//Build the font task
+gulp.task('build-font-svg', function()
+{
+  //Create the new font stream
+  var fontStream = new SVGIcons2SVGFontStream({ fontName: 'siimple-icons', normalize: true, fontHeight: 1000 });
+
+  //Setting the font destination
+  fontStream.pipe(fs.createWriteStream('dist/siimple-icons.font.svg'));
+
+  //Error event
+  fontStream.on('error', function(error){ console.log(error); });
+
+  //Read the icons list
+  utily.json.read('./icons.json', function(error, icons)
+  {
+    //Check the error
+    if(error){ throw error; }
+
+    //For each icon object in the list
+    icons.forEach(function(icon)
+    {
+      //Get the icon readable stream
+      var reader = fs.createReadStream(icon.path);
+
+      //Set the icon metadata
+      reader.metadata = { unicode: [ icon.unicode ], name: icon.id };
+
+      //Write the icon
+      fontStream.write(reader);
+    });
+
+    //End the stream
+    fontStream.end();
+  });
+});
+
+//Build font in ttf formt
+gulp.task('build-font-ttf', function()
+{
+  //Convert the svg font to ttf
+  gulp.src('./dist/siimple-icons.font.svg').pipe(svg2ttf()).pipe(gulp.dest('./dist'));
+});
+
+//Build font in woff2 format
+gulp.task('build-font-woff2', function()
+{
+  //Convert the ttf font to woff2
+  gulp.src('./dist/siimple-icons.font.ttf').pipe(ttf2woff2()).pipe(gulp.dest('./dist'));
 });
 
 //Build the svg sprite task
