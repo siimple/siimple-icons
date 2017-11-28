52a938e37da29ffd4ee4b4296dac0999c1790f80
jmjuanes <josemijuanes@gmail.com>
2017-11-28 16:13:41 +0100

    scripts/compile.js: minor changes

--- scripts/compile.js
+++ scripts/compile.js
@@ -4,11 +4,23 @@ var rename = require("gulp-rename");
 var handlebars = require('gulp-compile-handlebars');
 var utily = require('utily');
 
+//Import config
+var config = require('./config.js');
+
+//Initialize the new logger
+var log = config.logger();
+
+//Display in logs
+log.debug('Reading icons file...');
+
 //Read the icons files
 utily.json.read('./icons.json', function(error, icons)
 {
   //Check the error
   if(error){ throw error; }
+
+  //Display the number of icons in console
+  log.debug('Read ' + icons.length + ' icons');
 
   //Initialize the handlebars options
   var options = { helpers: {} };
@@ -16,10 +28,12 @@ utily.json.read('./icons.json', function(error, icons)
   //Add the unicode parser helper
   options.helpers.unicode_parser = function(value)
   {
-    //Return the unicode value parsed
-    //return '\\' + value.toString(16).toUpperCase();
-    return value;
+    //Return the parsed unicode value
+    return value.toString(16).toLowerCase();
   };
+
+  //Display in console
+  log.debug('Generating SCSS files...');
 
   //Select the scss templates
   gulp.src('./templates/**.scss.handlebars')
@@ -31,5 +45,9 @@ utily.json.read('./icons.json', function(error, icons)
   .pipe(rename({ extname: '' }))
 
   //Save to the scss folder
-  .pipe(gulp.dest('./scss'));
+  .pipe(gulp.dest('./scss').on('finish', function()
+  {
+    //Display in console
+    log.debug('Compiled files saved in ./scss folder')
+  }));
 });
