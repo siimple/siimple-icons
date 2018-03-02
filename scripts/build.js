dab9a9f053d87ff0049df7c0b4a69789e71b4052
jmjuanes <josemijuanes@gmail.com>
2018-03-02 11:27:50 +0100

    Big rewrite. Replaced gulp automation with vanilla JS

--- scripts/build.js
+++ scripts/build.js
@@ -1,209 +1,153 @@
-//Import dependencies
-var rmr = require('rmr');
-var fs = require('fs');
-var path = require('path');
-var gulp = require('gulp');
-var svgstore = require('gulp-svgstore');
-var svgmin = require('gulp-svgmin');
-var rename = require("gulp-rename");
-var SVGIcons2SVGFontStream = require('svgicons2svgfont');
-var svg2ttf = require('gulp-svg2ttf');
-var ttf2woff2 = require('gulp-ttf2woff2');
-var ttf2woff = require('gulp-ttf2woff');
-var utily = require('utily');
-var keue = require('keue');
-var sass = require('gulp-sass');
-var logty = require('logty');
+let rmr = require("rmr");
+let fs = require("fs");
+let path = require("path");
+let SVGIcons2SVGFontStream = require("svgicons2svgfont");
+let svg2ttf = require("svg2ttf");
+let ttf2woff2 = require("ttf2woff2");
+let ttf2woff = require("ttf2woff");
+let sass = require("node-sass");
+let mkdirp = require("mkdirp");
+let svgo = require("svgo");
+let svgstore = require("svgstore");
+let glob = require("glob");
 
-//Get the new logty instance
-var log = new logty(null);
+let tasks = require("./tasks.js");
 
-//Pipe to the console
-log.pipe(process.stdout);
+let icons = require("../icons.json");
 
-//Initialize the functions queue
-var k = new keue(function(next)
-{
-  //Display in logs
-  log.info('Cleaning folder ./dist');
+//Output paths
+let paths = {};
+paths.svgFont = "./dist/fonts/siimple-icons.font.svg";
+paths.ttfFont = "./dist/fonts/siimple-icons.font.ttf";
+paths.woffFont = "./dist/fonts/siimple-icons.font.woff";
+paths.woff2Font = "./dist/fonts/siimple-icons.font.woff2";
 
-  //Clean the dist folder
-  rmr.sync('./dist/');
+//Clean dist folder
+tasks.task("dist:clean", function (done) {
+    tasks.logger.log("Cleaning folder ./dist");
+    return rmr("./dist/", function (error) {
+        return done(error);
+    });
+});
 
-  //Continue with the next task
-  return next();
+//Create the dist folder
+tasks.task("dist:create", function (done) {
+    return mkdirp("./dist/fonts", function (error) {
+        return done(error);
+    })
 });
 
 //Compile SCSS files and generate the final CSS files
-k.then(function(next)
-{
-  //Print in console
-  log.info('Building CSS files...');
-
-  //Select all the SCSS files
-  return gulp.src('scss/**/*.scss')
-
-  //Compile the scss files
-  .pipe(sass().on('error', sass.logError))
-
-  //Pipe and save to the dist folder
-  .pipe(gulp.dest('./dist').on('finish', function()
-  {
-    //Print in console
-    log.info('CSS files saved in ./dist');
-
-    //Continue
-    return next();
-  }));
+tasks.task("compile:scss", function (done) {
+    return fs.readFile("./scss/siimple-icons.scss", "utf8", function (error, content) {
+        if (error) {
+            return done(error);
+        }
+        //Compile the scss file
+        return sass.render({data: content, includePaths: ["./scss/"]}, function (error, result) {
+            if (error) {
+                return done(error);
+            }
+            //Write the compiled css file
+            return fs.writeFile("./dist/siimple-icons.css", result.css, "utf8", function (error) {
+                return done(error);
+            });
+        });
+    });
 });
 
 //Build the svg sprite
-k.then(function(next)
-{
-  //Output stream
-  var writer = gulp.dest('./dist/');
-
-  //Finish event
-  writer.on('finish', next);
-
-  //Get the svg files
-  gulp.src('./svg/*.svg', { base: './src' })
-
-  //Minimize each file
-  .pipe(svgmin(function(file)
-  {
-    //Get the icon prefix
-    var prefix = path.basename(file.relative, path.extname(file.relative));
-
-    //Return the prefix plugin
-    return { plugins: [ { cleanupIDs: { prefix: prefix + '-',  minify: true } } ] };
-  }))
-
-  //Build the sprite
-  .pipe(svgstore())
-
-  //Rename the output file
-  .pipe(rename({ basename: 'siimple-icons' }))
-
-  //Save the sprite image
-  .pipe(writer);
-});
-
-//Create the fonts folder
-k.then(function(next)
-{
-  //Print in logs
-  log.info('Creating folder ./dist/fonts');
-
-  //Create the fonts folder
-  return utily.fs.mkdir('./dist/fonts', function(error)
-  {
-    //Check the error
-    if(error){ throw error; }
-
-    //Continue
-    return next();
-  });
+tasks.task("compile:svg", function (done) {
+    return glob("./svg/*.svg", function (error, files) {
+        if (error) {
+            return done(error);
+        }
+        //Sprites storage
+        let sprites = svgstore();
+        let parseSvg = function (index) {
+            if (index >= files.length) {
+                let output = "./dist/siimple-icons.svg";
+                return fs.writeFile(output, sprites, "utf8", function (error) {
+                    return done(error);
+                });
+            }
+            let file = files[index];
+            tasks.logger.log("Adding icon '" + file + "' to svg sprite");
+            return fs.readFile(file, "utf8", function (error, content) {
+                if (error) {
+                    return done(error);
+                }
+                //Initialize the svg minimize
+                let prefix = path.basename(file, path.extname(file));
+                let svgmin = new svgo({plugins: [{cleanupIDs: {prefix: prefix + '-', minify: true}}]});
+                return svgmin.optimize(content).then(function (result) {
+                    //Save to the svg sprite
+                    sprites.add(prefix, result);
+                    return parseSvg(index + 1);
+                });
+            });
+        };
+        return parseSvg(0);
+    });
 });
 
 //Build the SVG font
-k.then(function(next)
-{
-  //Create the new font stream
-  var fontStream = new SVGIcons2SVGFontStream({ fontName: 'SiimpleIcons', normalize: true, fontHeight: 1000 });
-
-  //Writer stream
-  var writer = fs.createWriteStream('dist/fonts/siimple-icons.font.svg');
-
-  //Print in logs
-  log.info('Creating svg font in ./dist/fonts');
-
-  //Pipe the font stream
-  fontStream.pipe(writer);
-
-  //Error event
-  fontStream.on('error', function(error){ console.log(error); });
-
-  //Read the icons list
-  utily.json.read('./icons.json', function(error, icons)
-  {
-    //Check the error
-    if(error){ throw error; }
-
-    //For each icon object in the list
-    icons.forEach(function(icon)
-    {
-      //Get the icon path
-      var icon_path = path.join('svg/', icon.id + '.svg');
-
-      //Get the icon readable stream
-      var reader = fs.createReadStream(icon_path);
-
-      //Set the icon metadata
-      reader.metadata = { unicode: [ String.fromCharCode(icon.unicode) ], name: icon.id };
-
-      //Write the icon
-      fontStream.write(reader);
+tasks.task("font:svg", function (done) {
+    let fontStream = new SVGIcons2SVGFontStream({fontName: "SiimpleIcons", normalize: true, fontHeight: 1000});
+    let writer = fs.createWriteStream(paths.svgFont);
+    fontStream.pipe(writer);
+    //Error listener
+    fontStream.on('error', function (error) {
+        return done(error);
     });
-
-    //End the stream
+    //Writer finished
+    writer.on('finish', function () {
+        tasks.logger.log("Font saved as " + paths.svgFont);
+        return done();
+    });
+    //Add each icon in the font stream
+    icons.forEach(function (icon) {
+        tasks.logger.log("Adding icon '" + icon.id + "' to SVG font");
+        let iconPath = path.join("./svg/", icon.id + ".svg");
+        let iconReader = fs.createReadStream(iconPath);
+        //Set the icon metadata
+        iconReader.metadata = {unicode: [String.fromCharCode(icon.unicode)], name: icon.id};
+        fontStream.write(iconReader);
+    });
     fontStream.end();
-  });
-
-  //Writer finished
-  writer.on('finish', function(){ return next(); });
 });
 
 //Build font in ttf format
-k.then(function(next)
-{
-  //Output stream
-  var writer = gulp.dest('./dist/fonts/');
-
-  //Print in logs
-  log.info('Creating ttf font in ./dist/fonts');
-
-  //Finish event
-  writer.on('finish', next);
-
-  //Convert the svg font to ttf
-  gulp.src('./dist/fonts/**.font.svg').pipe(svg2ttf()).pipe(writer);
+tasks.task("font:ttf", function (done) {
+    return fs.readFile(paths.svgFont, "utf8", function (error, content) {
+        if (error) {
+            return done(error);
+        }
+        //Convert the svg font to ttf font format
+        let ttf = svg2ttf(content, {});
+        tasks.logger.log("Saving ttf font as " + paths.ttfFont);
+        return fs.writeFile(paths.ttfFont, new Buffer(ttf.buffer), function (error) {
+            return done(error);
+        });
+    });
 });
 
 //Build font in woff format
-k.then(function(next)
-{
-  //Output stream
-  var writer = gulp.dest('./dist/fonts/');
-
-  //Print in logs
-  log.info('Creating woff font in ./dist/fonts');
-
-  //Finish event
-  writer.on('finish', next);
-
-  //Convert the ttf font to woff
-  gulp.src('./dist/fonts/**.font.ttf').pipe(ttf2woff()).pipe(writer);
+tasks.task("font:woff", function (done) {
+    let input = fs.readFileSync(paths.ttfFont);
+    let ttf = new Uint8Array(input);
+    let woff = new Buffer(ttf2woff(ttf, {}).buffer);
+    fs.writeFileSync(paths.woffFont, woff);
+    return done();
 });
 
 //Build font in woff2 format
-k.then(function(next)
-{
-  //Output stream
-  var writer = gulp.dest('./dist/fonts/');
-
-  //Print in logs
-  log.info('Creating woff2 font in ./dist/fonts');
-
-  //Finish event
-  writer.on('finish', next);
-
-  //Convert the ttf font to woff2
-  gulp.src('./dist/fonts/**.font.ttf').pipe(ttf2woff2()).pipe(writer);
+tasks.task("font:woff2", function (done) {
+    let input = fs.readFileSync(paths.ttfFont);
+    let ttf = new Uint8Array(input);
+    fs.writeFileSync(paths.woff2Font, ttf2woff2(ttf));
+    return done();
 });
 
-//Queue finished
-k.finish(function()
-{
-  log.info('Build completed');
-});
+//Run all tasks in order
+tasks.run();
