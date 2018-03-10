fcfc2c7e126a4a5cb7896e527fcf093a070df727
jmjuanes <josemijuanes@gmail.com>
2018-03-10 21:44:23 +0100

    Minor changes

--- scripts/build.js
+++ scripts/build.js
@@ -10,8 +10,7 @@ let mkdirp = require("mkdirp");
 let svgo = require("svgo");
 let svgstore = require("svgstore");
 let glob = require("glob");
-
-let tasks = require("./tasks.js");
+let flow = require("tinyflow");
 
 let icons = require("../icons.json");
 
@@ -23,22 +22,22 @@ paths.woffFont = "./dist/fonts/siimple-icons.font.woff";
 paths.woff2Font = "./dist/fonts/siimple-icons.font.woff2";
 
 //Clean dist folder
-tasks.task("dist:clean", function (done) {
-    tasks.logger.log("Cleaning folder ./dist");
+flow.task("dist:clean", function (done) {
+    flow.log("Cleaning folder ./dist");
     return rmr("./dist/", function (error) {
         return done(error);
     });
 });
 
 //Create the dist folder
-tasks.task("dist:create", function (done) {
+flow.task("dist:create", function (done) {
     return mkdirp("./dist/fonts", function (error) {
         return done(error);
     })
 });
 
 //Compile SCSS files and generate the final CSS files
-tasks.task("compile:scss", function (done) {
+flow.task("compile:scss", ["dist:clean", "dist:create"], function (done) {
     return fs.readFile("./scss/siimple-icons.scss", "utf8", function (error, content) {
         if (error) {
             return done(error);
@@ -57,7 +56,7 @@ tasks.task("compile:scss", function (done) {
 });
 
 //Build the svg sprite
-tasks.task("compile:svg", function (done) {
+flow.task("compile:svg", ["dist:clean", "dist:create"], function (done) {
     return glob("./svg/*.svg", function (error, files) {
         if (error) {
             return done(error);
@@ -72,7 +71,7 @@ tasks.task("compile:svg", function (done) {
                 });
             }
             let file = files[index];
-            tasks.logger.log("Adding icon '" + file + "' to svg sprite");
+            flow.log("Adding icon '" + file + "' to svg sprite");
             return fs.readFile(file, "utf8", function (error, content) {
                 if (error) {
                     return done(error);
@@ -92,8 +91,9 @@ tasks.task("compile:svg", function (done) {
 });
 
 //Build the SVG font
-tasks.task("font:svg", function (done) {
-    let fontStream = new SVGIcons2SVGFontStream({fontName: "SiimpleIcons", normalize: true, fontHeight: 1000});
+flow.task("font:svg", ["dist:clean", "dist:create"], function (done) {
+    let fontOptions = {fontName: "SiimpleIcons", normalize: true, fontHeight: 1000}; 
+    let fontStream = new SVGIcons2SVGFontStream(fontOptions);
     let writer = fs.createWriteStream(paths.svgFont);
     fontStream.pipe(writer);
     //Error listener
@@ -102,12 +102,12 @@ tasks.task("font:svg", function (done) {
     });
     //Writer finished
     writer.on('finish', function () {
-        tasks.logger.log("Font saved as " + paths.svgFont);
+        flow.log("Font saved as " + paths.svgFont);
         return done();
     });
     //Add each icon in the font stream
     icons.forEach(function (icon) {
-        tasks.logger.log("Adding icon '" + icon.id + "' to SVG font");
+        flow.log("Adding icon '" + icon.id + "' to SVG font");
         let iconPath = path.join("./svg/", icon.id + ".svg");
         let iconReader = fs.createReadStream(iconPath);
         //Set the icon metadata
@@ -118,14 +118,14 @@ tasks.task("font:svg", function (done) {
 });
 
 //Build font in ttf format
-tasks.task("font:ttf", function (done) {
+flow.task("font:ttf", ["font:svg"], function (done) {
     return fs.readFile(paths.svgFont, "utf8", function (error, content) {
         if (error) {
             return done(error);
         }
         //Convert the svg font to ttf font format
         let ttf = svg2ttf(content, {});
-        tasks.logger.log("Saving ttf font as " + paths.ttfFont);
+        flow.log("Saving ttf font as " + paths.ttfFont);
         return fs.writeFile(paths.ttfFont, new Buffer(ttf.buffer), function (error) {
             return done(error);
         });
@@ -133,21 +133,26 @@ tasks.task("font:ttf", function (done) {
 });
 
 //Build font in woff format
-tasks.task("font:woff", function (done) {
+flow.task("font:woff", ["font:ttf"], function (done) {
     let input = fs.readFileSync(paths.ttfFont);
     let ttf = new Uint8Array(input);
     let woff = new Buffer(ttf2woff(ttf, {}).buffer);
-    fs.writeFileSync(paths.woffFont, woff);
-    return done();
+    flow.log("Saving woff font as " + paths.woffFont);
+    return fs.writeFile(paths.woffFont, woff, function (error){
+        return done(error);
+    });
 });
 
 //Build font in woff2 format
-tasks.task("font:woff2", function (done) {
+flow.task("font:woff2", ["font:woff2"], function (done) {
     let input = fs.readFileSync(paths.ttfFont);
     let ttf = new Uint8Array(input);
-    fs.writeFileSync(paths.woff2Font, ttf2woff2(ttf));
-    return done();
+    flow.log("Saving woff2 font as " + paths.woff2Font);
+    return fs.writeFile(paths.woff2Font, ttf2woff2(ttf), function (error){
+        return done(error);
+    });
 });
 
 //Run all tasks in order
-tasks.run();
+flow.defaultTask(["compile:scss", "compile:svg", "font:svg", "font:ttf", "font:woff", "font:woff2"]);
+
